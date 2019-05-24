2c460eb604422ff6cc527dee5b0bf08ba32f366f
ruslancik <44032621+ruslancik@users.noreply.github.com>
2019-05-25 00:42:10 +0300

    exp and inc func constructions

--- js/app.js
+++ js/app.js
@@ -1,9 +1,35 @@
 // BUDGET CONTROLLER
 var budgetController = (function(){
 
-//some code
+var Expense = function(id,description,value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+}
+
+var Income = function(id,description,value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+}
+
+var date = {
+    
+    allItems : {
+        exp: [],
+        inc: []
+    },
+
+    total : {
+        exp: 0,
+        inc: 0
+    }
+}
+
+
 
 })();
+
 
 //UI CONTROLLER
 var UIController = (function(){
