c5e4c5c26ed2ac38c299e7042667074430bf6fdd
ruslancik <44032621+ruslancik@users.noreply.github.com>
2019-05-27 23:20:24 +0300

    updating budget finish

--- js/app.js
+++ js/app.js
@@ -241,6 +241,12 @@ var controller = (function( budgetCtrl, UICtrl){
     return {
         init: function(){
             console.log('Application has started !');
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc : 0,
+                totalExp : 0,
+                percentage: -1
+            })
             setUpEventListeners();
         }
     }
